35e55f2be47bf803ce742e93ab50efbe5c8a1eef
Henrique Ribeiro Rodrigues <henriquerrodrigues3@gmail.com>
2023-08-05 16:06:55 -0300

    Banco de Dados
    banco de dados(1)

---
+++ sge/application/banco/bazar.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Ago-2023 às 20:55
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bazar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `pdt_codigo` int(11) NOT NULL,
+  `pdt_descricao` text NOT NULL,
+  `pdt_valor` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usu_codigo` int(11) NOT NULL,
+  `usu_nome` text NOT NULL,
+  `usu_login` text NOT NULL,
+  `usu_senha` text NOT NULL,
+  `upe_codigo` int(11) NOT NULL,
+  `usu_situacao` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_permissao`
+--
+
+CREATE TABLE `usuario_permissao` (
+  `upe_codigo` int(11) NOT NULL,
+  `upe_descricao` text NOT NULL,
+  `upe_situacao` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `usuario_permissao`
+--
+
+INSERT INTO `usuario_permissao` (`upe_codigo`, `upe_descricao`, `upe_situacao`) VALUES
+(1, 'Administrador', 1),
+(2, 'Funcionário', 1),
+(3, 'Cliente', 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`pdt_codigo`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usu_codigo`);
+
+--
+-- Índices para tabela `usuario_permissao`
+--
+ALTER TABLE `usuario_permissao`
+  ADD PRIMARY KEY (`upe_codigo`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `pdt_codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `usu_codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario_permissao`
+--
+ALTER TABLE `usuario_permissao`
+  MODIFY `upe_codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
